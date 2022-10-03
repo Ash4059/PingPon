4ef62a62609d525add03b7e0f6a4ae419ac610f1
Ash4059 <53855674+Ash4059@users.noreply.github.com>
2022-10-03 22:07:21 +0530

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -4,7 +4,8 @@
 const rod1 = document.querySelector('.Bar-one');
 const rod2 = document.querySelector('.Bar-two');
 const ball = document.querySelector('.ball');
-const playBtn = document.querySelector('.play');
+const startPlayContainer = document.querySelector('.firstPlayContainer');
+const restartEndGame = document.querySelector('.restartEndGame');
 
 // Intervals for Rod and balls
 var setIntervalRod;
@@ -16,25 +17,56 @@ const animateTopRight = "animate-top-right";
 const animateBotttomLeft = "animate-bottom-left";
 const animateBotttomRight = "animate-bottom-right";
 
-
-// User variables
-const user1 = "User1";
-const user2 = "User2";
+// User Info
+var current_score =
+{
+    first: 0,
+    second: 0,
+};
+var UserName =
+{
+    first: "",
+    second: ""
+};
+var maxScore = 
+{
+    first:0,
+    second:0
+}
 
 let gameStarted = false;
 // Functions
 function startPlay()
 {
-    playBtn.style.visibility = "hidden";
-    playBtn.style.zIndex = '0';
+    if(gameStarted)
+    {
+        return;
+    }
+    const user1 = document.querySelector("#user1");
+    const user2 = document.querySelector("#user2");
+    if(user1.value == "" || user2.value == "")
+    {
+        window.alert("Please enter users name");
+        return;
+    }
+    UserName.first = user1.value;
+    UserName.second = user2.value;
+    gameStarted = true;
+    startPlayContainer.style.visibility = "hidden";
+    startPlayContainer.style.zIndex = '0';
     ball.classList.add(animateTopRight);
     moveBalls();
 }
 
 document.addEventListener('keydown',(event)=>{
-    console.log(event.keyCode);
+    
     switch(event.keyCode)
     {
+        case(13):
+        {
+            startPlay();
+            break;
+        }
         case(65):
         {
             moveRod("left");
@@ -55,16 +87,6 @@ document.addEventListener('keydown',(event)=>{
             moveRod("right");
             break;
         }
-        case(13):
-        {
-            if(gameStarted)
-            {
-                break;
-            }
-            gameStarted = true;
-            startPlay();
-            break;
-        }
         default:
         {
             moveRod("stop");
@@ -81,9 +103,15 @@ function moveBalls()
         {
             if((ballLeft<rod1.offsetLeft) || (ballLeft>(rod1.offsetWidth+rod1.offsetLeft)))
             {
-                endGame(user1);
+                restart();
                 return;
             }
+            else if(ballLeft >= rod1.offsetLeft || ballLeft <= (rod1.offsetWidth + rod1.offsetLeft))
+            {
+                current_score.first++;
+            }
+            
+            console.log(current_score.first,current_score.second);
             if(ball.classList.contains(animateTopLeft))
             {
                 ball.classList.remove(animateTopLeft);
@@ -99,10 +127,15 @@ function moveBalls()
         {
             if((ballLeft<rod1.offsetLeft) || (ballLeft>(rod1.offsetWidth+rod1.offsetLeft)))
             {
-                endGame(user2);
+                restart();
                 return;
             }
-
+            else if(ballLeft >= rod1.offsetLeft || ball <= (rod2.offsetWidth + rod2.offsetLeft))
+            {
+                current_score.second++;
+            }
+            
+            console.log(current_score.first,current_score.second);
             if(ball.classList.contains(animateBotttomRight))
             {
                 ball.classList.remove(animateBotttomRight);
@@ -140,7 +173,8 @@ function moveBalls()
                 ball.classList.add(animateBotttomLeft);
             }
         }
-    },1);
+        
+    },2);
 }
 
 function removeAllClassFromBall()
@@ -151,18 +185,55 @@ function removeAllClassFromBall()
     ball.classList.remove(animateTopRight);
 }
 
-function endGame(user)
+function restart()
 {
     clearInterval(setIntervalBalls);
     clearInterval(setIntervalRod);
     removeAllClassFromBall();
+    maxScore.first = Math.max(maxScore.first,current_score.first);
+    maxScore.second = Math.max(maxScore.second,current_score.second);
+    moveRodToCentral();
+    if(current_score.first > current_score.second)
+    {
+        window.alert(UserName.first + " is won!");
+    }
+    else
+    {
+        window.alert(UserName.second + " is won!");
+    }
+    restartEndGame.style.left = (window.innerWidth/2 - restartEndGame.offsetWidth/2) + "px";
+    restartEndGame.style.top = (window.innerHeight/2 - restartEndGame.offsetHeight/2) + "px";
+    restartEndGame.style.visibility = "visible";
+    restartEndGame.style.zIndex = 2;
+}
+
+function restartGame()
+{
+    restartEndGame.style.visibility = "hidden";
+    restartEndGame.style.zIndex = 0;
+    gameStarted = false;
+    startPlay();
+}
+
+function endGame()
+{
     resetAllElements();
-    window.alert(user + "Lost !");
+    current_score.first = 0;
+    current_score.second = 0;
+    UserName.first = "";
+    UserName.second = "";
+    window.alert("Max score of " + UserName.first + " is " + maxScore.first + ". Max score of " + UserName.second + " is " + maxScore.second);
+    maxScore.first = 0;
+    maxScore.second = 0;
     return;
 }
 
 function moveRod(direction)
 {
+    if(!gameStarted)
+    {
+        return;
+    }
     clearInterval(setIntervalRod);
     if(direction == "left")
     {
@@ -213,10 +284,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
 function resetAllElements()
 {
     gameStarted = false;
-    playBtn.style.left = (window.innerWidth/2 - playBtn.offsetWidth/2) + "px";
-    playBtn.style.top = (window.innerHeight/2 - playBtn.offsetHeight/2) + "px";
-    playBtn.style.visibility = "visible";
-    playBtn.style.zIndex = 2;
+    restartEndGame.style.visibility = "hidden";
+    restartEndGame.style.zIndex = 0;
+    startPlayContainer.style.left = (window.innerWidth/2 - startPlayContainer.offsetWidth/2) + "px";
+    startPlayContainer.style.top = (window.innerHeight/2 - startPlayContainer.offsetHeight/2) + "px";
+    startPlayContainer.style.visibility = "visible";
+    startPlayContainer.style.zIndex = 2;
+    moveRodToCentral();
+}
+
+function moveRodToCentral()
+{
     rod1.style.left = (window.innerWidth/2 - rod1.offsetWidth/2) + "px";
     rod2.style.left = (window.innerWidth/2 - rod2.offsetWidth/2) + "px";
     ball.style.left = (window.innerWidth/2 - ball.offsetWidth/2) + "px";
